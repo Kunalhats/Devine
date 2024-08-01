e6d69e94bb3785cd9f9bdc3cb0ce7796995100e8
Kunal Kumar Singh <109820362+Kunalhats@users.noreply.github.com>
2024-08-01 11:41:04 +0000

    Changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/devine/', // Replace '<repository>' with your GitHub repository name
+  base: '/Devine/', // Replace '<repository>' with your GitHub repository name
   build: {
     chunkSizeWarningLimit: 1000 // Set the limit to 1000 kB or higher
   }
