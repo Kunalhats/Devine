ceca8344d18b85252845484d69755f75081e8120
Kunal Kumar Singh <109820362+Kunalhats@users.noreply.github.com>
2024-08-01 11:07:32 +0000

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Replace with your repository name if it's different
+const base = '/Devine/';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: base,
+});
